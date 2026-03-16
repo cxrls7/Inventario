d20c0e7539f179d56688eefe6be764915c22d088
Carlos Molina <carlitos17abx@gmail.com>
2026-03-16 15:46:07 -0500

    Añadi las funciones faltantes

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from src.inventario import registrar_producto
-from src.inventario import buscar_producto
-from src.inventario import calcular_estadisticas
-from src.inventario import actualizar_producto
-import os
+from src.inventario import registrar_producto, buscar_producto, calcular_estadisticas, actualizar_producto, eliminar_producto
 import locale
+import os
 
 
 def formato_cop(valor):
     return locale.currency(valor, symbol=True, grouping=True)
+
+
 try:
     locale.setlocale(locale.LC_ALL, "es_CO.UTF-8")
-except:locale.setlocale(locale.LC_ALL, "SPANISH_COLOMBIA") 
+except:
+    locale.setlocale(locale.LC_ALL, "SPANISH_COLOMBIA")
 
- 
+
 def limpiar():
-     os.system("cls" if os.name == "nt" else "clear")
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 def main():
 
@@ -22,24 +23,23 @@ def main():
 
     while True:
 
-            print("\n--- BIENVENIDO AL INVENTARIO ---")
-            print()
-            print("-Sistema que te ayudara a registrar, actualizar o eliminar productos de tu inventario.")
-       
-            print("\n---- MENU DEL INVENTARIO ----")
-            print("\n1- Agregar producto")
-            print("2- Ver inventario")
-            print("3- Buscar producto")
-            print("4- Actualizar producto")
-            print("5- Eliminar producto")
-            print("6-📊 Ver estadisticas")
-            print("7- Salir")
+        print("\n--- BIENVENIDO AL INVENTARIO ---")
+        print()
+        print("-Sistema con diferentes funciones que te facilitara la gestion de los productos de tu inventario.")
 
-            opcion = input("\n-Ingresa una opcion: ")
-        
+        print("\n---- MENU DEL INVENTARIO ----")
+        print("\n1-📥 Agregar producto")
+        print("2-📦 Ver inventario")
+        print("3-🔎 Buscar producto")
+        print("4-💱 Actualizar producto")
+        print("5-⛔ Eliminar producto")
+        print("6-📊 Ver estadisticas")
+        print("7-👋 Salir")
 
-            if opcion == "1":
-             while True:
+        opcion = input("\n-Ingresa una opcion: ")
+
+        if opcion == "1":
+            while True:
                 limpiar()
                 nuevo_producto = registrar_producto()
                 inventario.append(nuevo_producto)
@@ -47,142 +47,111 @@ def main():
                 print("Resumen del registro")
                 print("=================================")
                 print(F"✅ Se registro: {nuevo_producto["nombre"]}")
-                print(f"📥 Cantidad: {nuevo_producto["cantidad"]}") 
+                print(f"📥 Cantidad: {nuevo_producto["cantidad"]}")
                 print(f"💰 Total final: {formato_cop(nuevo_producto["total"])}")
                 print("=================================")
                 print("\nProducto añadido correctamente ✅")
 
                 realizar_otra = input("\n¿Desea añadir otro producto? si/no: ")
                 if realizar_otra != "si":
-                 limpiar()
-                 break
-            
-                
-            elif opcion == "2":
-             limpiar()
-             if not inventario: 
-                    limpiar()
-                    print(" INVENTARIO VACIO 📂 ")
-                    input("\n-Presiona ENTER para regresar al menu...")
-                    limpiar()
-                    
-             else:
-                limpiar()
-                for nuevo_producto in inventario: print(f"| Nombre: {nuevo_producto["nombre"]} | Precio: {formato_cop(nuevo_producto["precio"])} | Cantidad: {nuevo_producto["cantidad"]} |")
-                pass
-                
-                input("\nPresiona ENTER para volver al menu...")
-                limpiar()
-            
-
-            elif opcion == "3":
-                
-                while True:
-                    limpiar()
-                    nombre = input("-Ingresa el nombre del producto: ")
-                    resultado = buscar_producto(inventario, nombre)
-                    
-                    if resultado:
-                    
-                         print("\n=================================")
-                         print("Resumen de busqueda")
-                         print("=================================")
-                         print(F"🏷️ Producto: {resultado["nombre"]}")
-                         print(f"🧺 Stock: {resultado["cantidad"]}") 
-                         print(f"💵 Valor total: {formato_cop(resultado["total"])}")
-                         print("=================================")
-
-                         input("¿Quieres hacer otra busqueda? si/no: ")
-                         if input != "si":
-                             limpiar()
-                             break
-                         
-                    
-                    else:
-                         print(f"\n ❕ EL PRODUCTO '{nombre}' NO SE HA ENCONTRADO ❕")
-                         input("\n Presiona ENTER para volver al menu...")
-                         limpiar()
-                         break
-
-                    
-            elif opcion == "4":
-                limpiar()
-                while True:
-                    actualizar_producto(inventario)
-                    input("Presiona ENTER para regresar al menu...")
                     limpiar()
                     break
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            elif opcion == "6":
+
+        elif opcion == "2":
+            limpiar()
+            if not inventario:
                 limpiar()
-                total_dinero, total_unidades = calcular_estadisticas(inventario)
-                
-                if not inventario:
-                    limpiar()
-                    print("❕NO HAY OBJETOS EN EL INVENTARIO❕")
-                    input("\nPresiona ENTER para salir...")
-                    limpiar()
-               
+                print(" INVENTARIO VACIO 📂 ")
+                input("\n-Presiona ENTER para regresar al menu...")
+                limpiar()
 
-
-                else:
-                 print("\n=================================")
-                 print("Reporte de inventario")
-                 print("=================================")
-                 print(f"🗒️ Numero de productos: {len(inventario)}")
-                 print(f"📋 Unidades totales: {total_unidades}") 
-                 print(f"💰 Valor total: {formato_cop(total_dinero)}")
-                 print("=================================")
-                 input("\nPresiona ENTER para volver al menu...")
-                 limpiar()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
-            
-            
-            
-            
-            
-            elif opcion == "7":
-             print("\nGracias por utilizar el sistema de inventario 👋 ")
-             break
             else:
                 limpiar()
-                print("❌ ERROR: Ingrese una opcion valida")
-        
+                print("---- MENU VISUALIZAR INVENTARIO ----")
+                for nuevo_producto in inventario:
+                    print(
+                        f"\n| Nombre: {nuevo_producto["nombre"]} | Precio: {formato_cop(nuevo_producto["precio"])} | Cantidad: {nuevo_producto["cantidad"]} |")
+                pass
+
+                input("\nPresiona ENTER para volver al menu...")
+                limpiar()
+
+        elif opcion == "3":
+
+            while True:
+                limpiar()
+                print("\n---- MENU BUSQUEDA DE PRODUCTOS ----")
+                nombre = input("\n-Ingresa el nombre del producto: ")
+                resultado = buscar_producto(inventario, nombre)
+
+                if resultado:
+
+                    print("\n=================================")
+                    print("Resumen de busqueda")
+                    print("=================================")
+                    print(F"🏷️ Producto: {resultado["nombre"]}")
+                    print(f"🧺 Stock: {resultado["cantidad"]}")
+                    print(f"💵 Valor total: {formato_cop(resultado["total"])}")
+                    print("=================================")
+
+                    input("-¿Quieres hacer otra busqueda? si/no: ")
+                    if input != "si":
+                        limpiar()
+                        break
+
+                else:
+                    print(f"\n ❕ EL PRODUCTO '{nombre}' NO SE HA ENCONTRADO ❕")
+                    input("\n Presiona ENTER para volver al menu...")
+                    limpiar()
+                    break
+
+        elif opcion == "4":
+            limpiar()
+            while True:
+                actualizar_producto(inventario)
+                salir = input("\n¿Quieres actualizar otro producto? si/no: ")
+                limpiar()
+                if salir == "no":
+                 limpiar()
+                 break
+
+        elif opcion == "5":
+            limpiar()
+            while True:
+                eliminar_producto(inventario)
+                respuesta=input("¿Deseas eliminar otro producto? si/no: ")
+                if respuesta == "no":
+                    limpiar()
+                    break
+
+        elif opcion == "6":
+            limpiar()
+            total_dinero, total_unidades = calcular_estadisticas(inventario)
+            print("---- MENU ESTADISTICAS DEL INVENTARIO ----")
+            if not inventario:
+                limpiar()
+                print("❕NO HAY OBJETOS EN EL INVENTARIO❕")
+                input("\nPresiona ENTER para salir...")
+                limpiar()
+
+            else:
+                print("\n=================================")
+                print("Reporte de inventario")
+                print("=================================")
+                print(f"🗒️ Numero de productos: {len(inventario)}")
+                print(f"📋 Unidades totales: {total_unidades}")
+                print(f"💰 Valor total: {formato_cop(total_dinero)}")
+                print("=================================")
+                input("\nPresiona ENTER para volver al menu...")
+                limpiar()
+
+        elif opcion == "7":
+            print("\nGracias por utilizar el sistema de inventario 👋 ")
+            break
+        else:
+            limpiar()
+            print("❌ ERROR: Ingrese una opcion valida")
+
 
 if __name__ == "__main__":
-     main()
-
+    main()
--- src/inventario.py
+++ src/inventario.py
@@ -1,12 +1,11 @@
 
-
 def calcular_total(precio,  cantidad):
     return precio * cantidad
     
 def registrar_producto():
-
+    print("---- MENU AÑADIR PRODUCTOS ----")
     
-    nombre = input("-Ingresa el nombre del producto: ")
+    nombre = input("\n-Ingresa el nombre del producto: ")
     
     
 
@@ -51,35 +50,66 @@ def calcular_estadisticas(inventario):
     return valor_total_acumulado, cantidad_total_items
     
 def actualizar_producto(inventario):
+   print("--- MENU ACTUALIZAR PRODUCTO ----")
 
-   nombre_buscar = input("Ingrese el nombre del producto a actualizar:").strip().lower()
+   nombre_buscar = input("\n-Ingrese el nombre del producto a actualizar:").strip().lower()
    
    for producto in inventario:
             
             if producto['nombre'].lower() == nombre_buscar:
-                    print(f"Producto encontrado {producto['nombre']}")
-                    print(f"Parametros actuales {producto['nombre']} || {producto['cantidad']} || {producto['precio']} ")
+                    print(f"\n-Producto encontrado ✔️ : {producto['nombre']}")
+                    print("\n-Parametros actuales")
+                    print("\nNombre          Cantidad          Precio")
+                    print("------------------------------------------")
+
+                    print(f"{producto['nombre']:10} {producto['cantidad']:10} {producto['precio']:20}")
 
             try:
             
             
-              nueva_cantidad = int(input("Ingrese la nueva cantidad: "))
-              nuevo_precio = float(input("Ingrese el nuevo precio: "))
+              nueva_cantidad = int(input("\n-Ingrese la nueva cantidad: "))
+              nuevo_precio = float(input("-Ingrese el nuevo precio: "))
 
               producto['cantidad'] = nueva_cantidad
               producto['precio'] = nuevo_precio
               producto['total'] = nueva_cantidad * nuevo_precio
 
-              print("\nProducto actualizado con exito")
+              print("\nProducto actualizado con exito ✅ ")
               return True
                 
             except ValueError:
-                    print("ERROR. INGRESA NUMEROS VALIDOS")
+                    print("❌ ERROR: INGRESA NUMEROS VALIDOS")
                     return False
                 
-   print(f"EL PRODUCTO {nombre_buscar} NO EXISTE")
+   print(f"\n❕ EL PRODUCTO {nombre_buscar} NO EXISTE ❕")
    return False        
 
+def eliminar_producto(inventario):
+        print("---- MENU ELIMINAR PRODUCTOS ----")
+        print(f"\n-Inventario actual 📦 ")
+        for producto in inventario:
+             print(f"\nNombre | {producto['nombre']} ")
+             
+             pass
+  
+        nombre_eliminar = input("\n-Ingresa el nombre del producto a eliminar: ")
+
+        for producto in inventario: 
+            try:
+                if producto ['nombre'] == nombre_eliminar:
+                    inventario.remove(producto)
+                    print("Producto eliminado correctamente ✅ ")
+                    return True
+            except ValueError:
+                print("❌ ERROR: Ingresa un nombre valido")
+
+        print(f"\n❕ EL PRODUCTO {nombre_eliminar} NO ESTA DISPONIBLE ❕")
+        return False
+         
+            
+       
+        
+    
 
 
 
